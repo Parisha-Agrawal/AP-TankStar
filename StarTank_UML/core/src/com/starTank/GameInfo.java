a519bbe88f6d7df260b596f84ab3a512a7fdcc64
Isha <52040505+Parisha-Agrawal@users.noreply.github.com>
2022-11-22 13:13:49 +0530

    Add files via upload

---
+++ StarTank_UML/core/src/com/starTank/GameInfo.java
@@ -0,0 +1,9 @@
+package com.starTank;
+
+public class GameInfo {
+    private final Player player1 = new Player();
+    private final Player player2 = new Player();
+    private final PlayerGameInfo P1GameInfo = player1.getInfo();
+    private final PlayerGameInfo P2GameInfo = player2.getInfo();
+
+}
---
+++ StarTank_UML/core/src/com/starTank/Shoot.java
@@ -0,0 +1,31 @@
+package com.starTank;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+public class Shoot extends Game {
+
+	public SpriteBatch batch;
+	public BitmapFont font;
+
+	public void create() {
+		batch = new SpriteBatch();
+		font = new BitmapFont(); // use libGDX's default Arial font
+		this.setScreen(new MainMenu(this));
+	}
+
+	public void render() {
+		super.render(); // important!
+	}
+
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+
+	public void remove() {
+
+	}
+}
